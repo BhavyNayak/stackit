aa13b5f92194d08e4f761a1f02b17e94480a884d
Bhavy Nayak <nayakbhavy132@gmail.com>
2025-07-12 14:32:36 +0530

    routes initiated

--- routes/answer_routes.py
+++ routes/answer_routes.py
@@ -3,8 +3,11 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from utils.database_helper import get_db
 from models import Answer
-
+from routes import answer_routes, question_routes, user_routes
 router = APIRouter()
+router.include_router(answer_routes.router)
+router.include_router(question_routes.router)
+router.include_router(user_routes.router)
 
 @router.get("/")
 def get_answers(db: Session = Depends(get_db)):
